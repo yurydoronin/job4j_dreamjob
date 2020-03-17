db83b1ce1ff148e28a2b7b5254d20cd7b5984d8c
Yury Doronin <doronin.ltd@gmail.com>
2020-03-17 13:47:09 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+[![Build Status](https://travis-ci.org/yurydoronin/job4j_dreamjob.svg?branch=master)](https://travis-ci.org/yurydoronin/job4j_dreamjob)
+[![codecov](https://codecov.io/gh/yurydoronin/job4j_dreamjob/branch/master/graph/badge.svg)](https://codecov.io/gh/yurydoronin/job4j_dreamjob)
 # job4j_dreamjob
+
+- Servlets & JSP
+- Hibernate
+- Spring
---
+++ src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,25 @@
+package ru.job4j.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class EchoServlet.
+ *
+ * @author Yury Doronin (doronin.ltd@gmail.com)
+ * @version 1.0
+ * @since 21.11.2019
+ */
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
