43ebee84c4daa9ffb419876dd790e54b079b6da1
Yury Doronin <doronin.ltd@gmail.com>
2020-04-30 13:44:56 +0300

    2. Filter [#2517]

---
+++ src/main/java/ru/job4j/dream/filter/AuthFilter.java
@@ -0,0 +1,33 @@
+package ru.job4j.dream.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
